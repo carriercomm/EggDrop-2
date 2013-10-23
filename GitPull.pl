2c71375430d1ea6b2ca4cb9748f6a7f204445c9e
ev0x <ev0x@phrack.com.au>
2013-10-23 20:30:23 +1000

    Version 1.0 of GitPull.pl

--- GitPull.pl
+++ GitPull.pl
@@ -8,7 +8,7 @@ use HTML::TagParser;
 my $site_head = "https://github.com/";
 
 #list your users to follow
-@users = ("Squire.atom","ev0x.atom");
+@users = ("Squire.atom","ev0x.atom","jawr.atom","omegaservices.atom");
 
 foreach my $user ( @users ) {
 	#create teh url
@@ -23,7 +23,7 @@ foreach my $user ( @users ) {
 		($found) = 0;
 		open(GITLOG, 'git.txt');
 		if (grep{/$id/i} <GITLOG>) {
-			print "found skipping..\n";
+#			print "found skipping..\n";
 			($found) = 1;
 		} else {
 			($raw_html) = $_->content->body, "\n";
@@ -49,21 +49,37 @@ foreach my $user ( @users ) {
 				}
 			}
 			if (grep{/master/i} $title) {
-				($rep) = "Master";
+				($branch) = "Master";
 			}
 			if (grep{/created repository/i} $title) {
-                                ($repname) = $title =~ /created repository (.*?)$/m;
-                                ($title) = "NEW REPO: $repname";
-                        }
-
+				($branch) = "Master";
+				($rep) = $title =~ /created repository (.*?)$/m;
+				($title) = "NEW REPO: $rep";
+			} elsif (grep{/created branch/i} $title) {
+				($branch) = $title =~ /created branch (.*?)  at/m;
+				($rep) = $title =~ /\/(.*?)$/m;
+				($title) = "NEW BRANCH in $rep";
+			} elsif (grep{/pushed to/i} $title) {
+				($branch) = $title =~ /pushed to (.*?) at/m;
+			} elsif (grep{/commented on/i} $title) {
+				($bypass) = 1;
+			} elsif (grep{/opened pull request/i} $title) {
+				($bypass) = 1;
+			}
 			$commiturl = "https://github.com$committail";
 			$gitio = `curl -s -i http://git.io -F "url=$commiturl" | grep Location | awk -F ":" '{print $2}'`;
-                        if (grep{/NEW REPO/i} $title) {
-                                ($where) = $title
-                        } else {
-                                ($where) = $title =~ /\/(.*?)$/m;
-                        }
-			print "[GitHub] $where by $author :: [$rep] $rev :: $comment :: $gitio\n";
+			if (grep{/NEW REPO/i} $title) {
+				($where) = $title;
+			} elsif (grep{/NEW BRANCH/i} $title) {
+				($where) = $title;
+			} else {
+				($where) = $title =~ /\/(.*?)$/m;
+			}
+			if ( "$bypass" == 1 ) {
+				print "[\x0311GitHub\x03] \x02$title :: $comment\x02\n";
+			} else {
+				print "[\x0311GitHub\x03] \x02$where\x02 \x0314by\x03 \x02$author\x02 :: [\x0304$branch\x03] \x02$rev\x02 :: \x02$comment\x02 :: \x0304$gitio\x03";
+			}
 		}
 		close (GITLOG);
 		if ($found == 0) {
