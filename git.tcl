08fe814bde64343089bcb9231d4f743389fb6b74
ev0x <ev0x@phrack.com.au>
2013-10-23 20:46:36 +1000

    Create git.tcl

---
+++ git.tcl
@@ -0,0 +1,22 @@
+set scriptpath_git "/home/eDrop/win0na/scripts/git/"
+
+bind time - "?1 * * * *" git_announce
+bind time - "?2 * * * *" git_announce
+bind time - "?3 * * * *" git_announce
+bind time - "?4 * * * *" git_announce
+bind time - "?5 * * * *" git_announce
+bind time - "?6 * * * *" git_announce
+bind time - "?7 * * * *" git_announce
+bind time - "?8 * * * *" git_announce
+bind time - "?9 * * * *" git_announce
+bind time - "?0 * * * *" git_announce
+
+proc git_announce { min hour day month year } {
+  putlog "execute git announce"
+  global scriptpath_git
+  set result [exec $scriptpath_git/gitpull.pl]
+  set result [split $result \n]
+  foreach line $result {
+    putquick "PRIVMSG #git :$line"
+  }
+}
